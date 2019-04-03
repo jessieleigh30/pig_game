71bc33fe33461a8ab479ef474c8d94331c836608
Jessica Anderson <jessie.leigh30@gmail.com>
2019-04-03 15:52:51 -0600

    player total scores work

--- app.js
+++ app.js
@@ -39,23 +39,51 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
     
   } else {
     //next player
-    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
-    roundScore = 0;
-
-    document.getElementById('current-0').textContent = '0';
-    document.getElementById('current-1').textContent = '0';
-
-    document.querySelector('.player-0-panel').classList.toggle('active');
-    document.querySelector('.player-1-panel').classList.toggle('active');
-
-    //make panel active for player whose turn it is
-    // document.querySelector('.player-0-panel').classList.remove('active');
-    // document.querySelector('.player-1-panel').classList.add('active');
-
-    document.querySelector('.dice').style.display = 'none';
+   nextPlayer();
   }
 
 });
+
+document.querySelector('.btn-hold').addEventListener('click', function(){
+  //1. add current score to the global score
+  scores[activePlayer] += roundScore;
+
+  // update the user interface
+  document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+ 
+  //check if player won the game
+
+ if (scores[activePlayer] >= 10) {
+   document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
+   document.querySelector('.dice').style.display = 'none';
+   document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+   document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+
+ } else {
+   nextPlayer();
+
+ }
+
+ 
+
+});
+
+function nextPlayer () {
+  activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+  roundScore = 0;
+
+  document.getElementById('current-0').textContent = '0';
+  document.getElementById('current-1').textContent = '0';
+
+  //make panel active for player whose turn it is
+  document.querySelector('.player-0-panel').classList.toggle('active');
+  document.querySelector('.player-1-panel').classList.toggle('active');
+
+  // document.querySelector('.player-0-panel').classList.remove('active');
+  // document.querySelector('.player-1-panel').classList.add('active');
+
+  document.querySelector('.dice').style.display = 'none';
+}
 
 
 // document.querySelector('#current-' + activePlayer ).textContent = dice;
