ee0f97227d1754b74a978d609e3e21ff13a25ae7
Jessica Anderson <jessie.leigh30@gmail.com>
2019-04-03 10:47:43 -0600

    active player toggles

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var scores, roundScore, activePlayer;
 
 scores=[0,0];
 roundScore = 0;
-activePlayer= 1;
+activePlayer= 0;
 
 
 document.querySelector('.dice').style.display = 'none';
@@ -23,16 +23,37 @@ document.getElementById('current-0').textContent = '0';
 document.getElementById('current-1').textContent = '0';
 
 document.querySelector('.btn-roll').addEventListener('click', function() {
-  //do something here
+  
   //1. random number
   var dice = Math.floor(Math.random() * 6) + 1;
   //2. display result
   var diceDOM = document.querySelector('.dice');
     diceDOM.style.display= 'block';
     diceDOM.src = 'dice-' + dice + '.png';
-
-
   //3. update the round score IF the rolled number was not a 1
+
+  if ( dice !== 1 ) {
+    //add score
+    roundScore += dice;
+    document.querySelector('#current-' + activePlayer ).textContent = roundScore;
+    
+  } else {
+    //next player
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    roundScore = 0;
+
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+
+    //make panel active for player whose turn it is
+    // document.querySelector('.player-0-panel').classList.remove('active');
+    // document.querySelector('.player-1-panel').classList.add('active');
+
+    document.querySelector('.dice').style.display = 'none';
+  }
 
 });
 
